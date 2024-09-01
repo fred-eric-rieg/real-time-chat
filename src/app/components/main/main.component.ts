e933f9885e8268620ff8fff157eb608be065b799
Frederic Rieg <fredericrieg@protonmail.com>
2024-09-01 19:21:43 +0300

    implemented sidemenu for main content area (for channels & direct msg)

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
+  @ViewChild('leftmenu') leftmenu!: ElementRef;
+  isShrunk = false;
+
+  constructor(private renderer: Renderer2) {};
+
+
+  toggleLeftMenu() {
+    const element = this.leftmenu.nativeElement;
+    if (element.classList.contains('shrink')) {
+      this.renderer.removeClass(element, 'shrink');
+      this.isShrunk = false;
+    } else {
+      this.renderer.addClass(element, 'shrink');
+      this.isShrunk = true;
+    }
+  }
   
 }
