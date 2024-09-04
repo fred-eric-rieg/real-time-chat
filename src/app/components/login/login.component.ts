0f9cd64ac928fc0c5750f88e181b1e84c808e38a
Frederic Rieg <fredericrieg@protonmail.com>
2024-09-04 13:05:11 +0300

    fixed typo in login error message for pw

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -15,20 +17,50 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+
+  private _snackBar = inject(MatSnackBar);
 
   loginForm = new FormGroup({
     emailFormControl: new FormControl('', [Validators.required, Validators.email]),
     passwordFormControl: new FormControl('', [Validators.required])
   });
 
+  routeSub!: Subscription;
 
-  constructor(private router: Router) {}
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    
+  }
+
+
+  ngOnInit(): void {
+    this.routeSub = this.route.queryParams.subscribe(params => {
+      let emailSnackbar = params['emailSnackbar'];
+      if (emailSnackbar) {
+        this.openSnackBar("Check your emails before first login.", "Mkay", {duration: 5000});
+      }
+    });
+  }
+
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
 
 
   login() {
     this.router.navigate(["/dashboard/channel/1"]);
   }
 
+
+  goToSignup() {
+    this.router.navigate(["/signup"])
+  }
+
+
+  openSnackBar(message: string, action: string, arg2: { duration: number; }) {
+    this._snackBar.open(message, action, arg2);
+  }
 
 }
