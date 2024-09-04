90ddd01fc00b8497e41ad21d5cda4880bad49adf
Frederic Rieg <fredericrieg@protonmail.com>
2024-09-04 13:04:58 +0300

    implemented signup form & custom validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AccountComponent } from './components/dashboard/account/account.component';
 import { AuthGuard } from './guard/auth.guard';
 import { ChannelComponent } from './components/dashboard/channel/channel.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: 'full' },
     { path: "login", component: LoginComponent },
+    { path: "signup", component: SignupComponent },
     { path: "dashboard", component: DashboardComponent , children: [
         { path: "channel/:id", component: ChannelComponent, canActivate: [AuthGuard] },
         { path: "account", component: AccountComponent, canActivate: [AuthGuard]}
---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,55 @@
+import { Component, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [MatCardModule, MatInputModule, MatButtonModule, FormsModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, CommonModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+  @ViewChild('signupBtn') signupBtn!: MatButton;
+
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    fullName: new FormControl('', [Validators.required]),
+    password: new FormControl('', Validators.required),
+    repeat: new FormControl('', [Validators.required])
+  }, { validators: [this.passwordsMatchValidator] });
+
+
+  constructor(private router: Router, private fb: FormBuilder) {}
+
+
+  passwordsMatchValidator(form: AbstractControl): ValidationErrors | null {
+    const password1 = form.get("password")?.value;
+    const password2 = form.get("repeat")?.value;
+
+    const noMatch = password1 !== password2;
+
+    if ((password1 && password2) && noMatch) {
+      return { nomatch: true };
+    }
+    return null;
+  }
+
+
+  signup() {
+    this.router.navigate(["/login"], { queryParams: { emailSnackbar: true }});
+  }
+
+
+  returnToLogin() {
+    this.router.navigate(["/login"]);
+  }
+}
