3fa1038fafae4e4c148e0050eb2ad38dfba0df76
Frederic Rieg <fredericrieg@protonmail.com>
2024-09-05 20:10:15 +0300

    added basic selection with autocomplete for direct message dialog

--- src/app/components/dialogs/write-direct-message/write-direct-message.component.ts
+++ src/app/components/dialogs/write-direct-message/write-direct-message.component.ts
@@ -1,16 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, Signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef } from '@angular/material/dialog';
+import { DataService, ShortMember } from '../../../shared/services/data.service';
+import { map, Observable, startWith } from 'rxjs';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'writeDirectMessage',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, FormsModule, ReactiveFormsModule, MatInputModule, MatAutocompleteModule, AsyncPipe],
   templateUrl: './write-direct-message.component.html',
   styleUrl: './write-direct-message.component.scss'
 })
-export class WriteDirectMessageComponent {
+export class WriteDirectMessageComponent implements OnInit {
+  myControl = new FormControl<string | ShortMember>('');
 
-  constructor(public dialogRef: MatDialogRef<WriteDirectMessageComponent>) {}
+  members: Signal<ShortMember[] | null> = signal(null);
+
+  filteredOptions!: Observable<ShortMember[] | undefined>;
+
+  constructor(public dialogRef: MatDialogRef<WriteDirectMessageComponent>, private dataService: DataService) {}
+
+
+  ngOnInit() {
+    this.dataService.fetchContacts();
+    this.members = this.dataService.getContacts();
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.fullName;
+        return name ? this._filter(name as string) : this.members()?.slice();
+      }),
+    );
+  }
+
+
+  displayFn(user: ShortMember): string {
+    return user && user.fullName ? user.fullName : '';
+  }
+
+
+  private _filter(name: string): ShortMember[] {
+    const filterValue = name.toLowerCase();
+
+    return this.members()?.filter(option => option.fullName.toLowerCase().includes(filterValue)) || [{ fullName: '', id: 0, image: ''}];
+  }
+
+
+  writeContact(contact: ShortMember) {
+    console.log("Writing to: ", contact);
+    this.dialogRef.close();
+  }
 
 }
