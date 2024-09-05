b1e06549be41f1e5273963c7863c91756668caed
Frederic Rieg <fredericrieg@protonmail.com>
2024-09-05 19:22:50 +0300

    created basic form for join channel dialog

--- src/app/components/dialogs/join-channel/join-channel.component.ts
+++ src/app/components/dialogs/join-channel/join-channel.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Signal, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { Channel } from '../../../shared/services/data.service';
 
 @Component({
   selector: 'app-join-channel',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule, CommonModule],
   templateUrl: './join-channel.component.html',
   styleUrl: './join-channel.component.scss'
 })
 export class JoinChannelComponent {
 
+  channelForm = new FormGroup({
+    name: new FormControl('', [Validators.required])
+  });
+
+  channels: Signal<Channel[]> = signal([ {id: 0, name: "unset", created: new Date(), members: [], description: "nothing", createdBy: {fullName: "no", id: 0, image: ""} } ]);
+
   constructor(public dialogRef: MatDialogRef<JoinChannelComponent>) {}
+
+
+  joinChannel() {
+    console.log(this.channelForm.value);
+    this.dialogRef.close();
+  }
 
 }
