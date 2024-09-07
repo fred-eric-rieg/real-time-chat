bfebff673cb8170ee5e42c9ef1345e7c8bc432b1
Frederic Rieg <fredericrieg@protonmail.com>
2024-09-07 10:10:35 +0300

    implemented create, login and logout for authService & injected firebaseApp (important)

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { MemberService } from './member.service';
+import { FirebaseStorage } from '@angular/fire/storage';
+import { FirebaseApp } from '@angular/fire/app';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  app: FirebaseApp = inject(FirebaseApp)
+
+  constructor(private memberService: MemberService) { }
+
+
+  async create(email: string, password: string, fullName: string) {
+    const auth = getAuth(this.app);
+    return createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Created
+        const user = userCredential.user;
+        this.memberService.setNewUser(user.uid, fullName, email, "");
+        return user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
+
+  async login(email: string, password: string) {
+    const auth = getAuth(this.app);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
+
+  async logout() {
+    const auth = getAuth(this.app);
+    return signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
+
 }
