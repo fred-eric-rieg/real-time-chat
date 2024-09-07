36b6b3db6331ad8f8b8a32bafeb4c53a795884ed
Frederic Rieg <fredericrieg@protonmail.com>
2024-09-07 10:09:49 +0300

    app config providers for Firebase Auth, Database and Storage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,12 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideQuillConfig } from 'ngx-quill';
 
-import { importProvidersFrom } from '@angular/core'; // maybe not needed...
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getDatabase, provideDatabase} from '@angular/fire/database';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from '../env/environment.development';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,15 +22,18 @@ export const appConfig: ApplicationConfig = {
         toolbar: [
           ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
 
-          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      
+          [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+
           ['clean'],                                         // remove formatting button
-      
+
           ['image']                         // link and image, video
         ]
       }
     }),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideDatabase(() => getDatabase())
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+    provideAuth(() => getAuth()),
   ]
 };
+
